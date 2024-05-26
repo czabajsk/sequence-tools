794633fc9d6c1d1cd04b4790783dfaccc36d03dd
joannaczabajska <joanna.czabajska@gmail.com>
2024-05-26 12:52:55 +0200

    TASK-4 adding plot

--- src/app/app.py
+++ src/app/app.py
@@ -3,6 +3,7 @@ Web app
 """
 from dash import Dash, html, dcc, callback, Output, Input, State
 from dash.exceptions import PreventUpdate
+import dash_bio as dashbio
 from src.features.needleman_wunsch import needleman_wunsch
 
 app = Dash()
@@ -13,6 +14,8 @@ app.layout = [
     dcc.Input(placeholder="Sequence B", type="text", required=True, id="seq_2"),
     html.Button("Generate", id="generate_alignment"),
     html.Pre(id="body-div"),
+    html.Div(id='default-alignment-viewer-output'),
+    html.Div(id="output-container")
 ]
 
 
@@ -21,10 +24,10 @@ app.layout = [
     [Input("generate_alignment", "n_clicks")],
     [State("seq_1", "value"), State("seq_2", "value")],
 )
-def update_graph(n_clicks, seq_1, seq_2):
+def generate_raw_alignment(n_clicks, seq_1, seq_2):
     """
     Display raw alignment
-    :param n_clicks: button click
+    :param n_clicks: button clicks
     :param seq_1: sequence one
     :param seq_2: sequence two
     :return: alignment with default parameters
@@ -32,6 +35,35 @@ def update_graph(n_clicks, seq_1, seq_2):
     if n_clicks is None:
         raise PreventUpdate
     return needleman_wunsch(seq_1, seq_2)
+
+@callback(
+    Output("output-container", "children"),
+    [Input("generate_alignment", "n_clicks")],
+    [State("seq_1", "value"),
+     State("seq_2", "value")],
+)
+def plot_alignment_chart(n_clicks, seq_1, seq_2):
+    """
+    Plot interactive chart
+    :param n_clicks: button clicks
+    :param seq_1: first sequence
+    :param seq_2: second sequence
+    :return: widget
+    """
+    if n_clicks is None:
+        return 'No data.'
+    data = f""">
+    {seq_1}
+    >
+    {seq_2}
+    """
+
+    return dashbio.AlignmentChart(
+        id='my-default-alignment-viewer',
+        data=data,
+        height=500,
+        tilewidth=30,
+    )
 
 
 if __name__ == "__main__":
