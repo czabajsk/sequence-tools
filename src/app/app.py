8d9e0c796cb5adf45d6d88cef78083b7d66c6969
joannaczabajska <joanna.czabajska@gmail.com>
2024-05-26 11:29:17 +0200

    TASK-4 raw alignment display

---
+++ src/app/app.py
@@ -0,0 +1,31 @@
+from dash import Dash, html, dcc, callback, Output, Input, State
+from dash.exceptions import PreventUpdate
+from src.features.needleman_wunsch import needleman_wunsch
+
+app = Dash()
+
+app.layout = [
+    html.H1(children='Needleman-Wunsch Demo', style={'textAlign': 'center'}),
+    dcc.Input(placeholder="Sequence A", type="text", required=True, id="seq_1"),
+    dcc.Input(placeholder="Sequence B", type="text", required=True, id="seq_2"),
+    html.Button('Generate', id='generate_alignment'),
+    html.Pre(id='body-div')
+]
+
+
+@callback(
+    Output('body-div', 'children'),
+    [Input('generate_alignment', 'n_clicks')],
+    [State('seq_1', 'value'),
+     State('seq_2', 'value')]
+
+)
+def update_graph(n_clicks, seq_1, seq_2):
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        return needleman_wunsch(seq_1, seq_2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
