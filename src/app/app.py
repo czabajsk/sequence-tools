60a24ccc80c4c62250f0e371f3cb2c2fec0b3a60
joannaczabajska <joanna.czabajska@gmail.com>
2024-05-26 11:32:49 +0200

    TASK-4 formatting

--- src/app/app.py
+++ src/app/app.py
@@ -1,3 +1,6 @@
+"""
+Web app
+"""
 from dash import Dash, html, dcc, callback, Output, Input, State
 from dash.exceptions import PreventUpdate
 from src.features.needleman_wunsch import needleman_wunsch
@@ -5,27 +8,31 @@ from src.features.needleman_wunsch import needleman_wunsch
 app = Dash()
 
 app.layout = [
-    html.H1(children='Needleman-Wunsch Demo', style={'textAlign': 'center'}),
+    html.H1(children="Needleman-Wunsch Demo", style={"textAlign": "center"}),
     dcc.Input(placeholder="Sequence A", type="text", required=True, id="seq_1"),
     dcc.Input(placeholder="Sequence B", type="text", required=True, id="seq_2"),
-    html.Button('Generate', id='generate_alignment'),
-    html.Pre(id='body-div')
+    html.Button("Generate", id="generate_alignment"),
+    html.Pre(id="body-div"),
 ]
 
 
 @callback(
-    Output('body-div', 'children'),
-    [Input('generate_alignment', 'n_clicks')],
-    [State('seq_1', 'value'),
-     State('seq_2', 'value')]
-
+    Output("body-div", "children"),
+    [Input("generate_alignment", "n_clicks")],
+    [State("seq_1", "value"), State("seq_2", "value")],
 )
 def update_graph(n_clicks, seq_1, seq_2):
+    """
+    Display raw alignment
+    :param n_clicks: button click
+    :param seq_1: sequence one
+    :param seq_2: sequence two
+    :return: alignment with default parameters
+    """
     if n_clicks is None:
         raise PreventUpdate
-    else:
-        return needleman_wunsch(seq_1, seq_2)
+    return needleman_wunsch(seq_1, seq_2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
