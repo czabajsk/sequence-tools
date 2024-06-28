f1bf90ba6cb878cb0197cca0c007bd4419d07440
Ola G. <ola.ank77@gmail.com>
2024-06-28 16:52:26 +0200

    Update home page - adding default inputs (match, mismatch, gap)

--- src/app/home_page.py
+++ src/app/home_page.py
@@ -13,26 +13,65 @@ def create_home_layout() -> dbc.Container:
     return dbc.Container([
         html.Div(style={"height": "70px"}),  # Spacer
         html.H1(children="Needleman-Wunsch Demo", style={"textAlign": "center"}),
-        dbc.Input(placeholder="Sequence A",
-                  type="text",
-                  size="lg",
-                  className="mb-3",
-                  required=True,
-                  id="seq_1"),
-        dbc.Input(placeholder="Sequence B",
-                  type="text",
-                  size="lg",
-                  className="mb-3",
-                  required=True,
-                  id="seq_2"),
-        dbc.Button("Generate",
-                   id="generate_alignment",
-                   color="info",
-                   size="lg"),
-        html.Pre(id="body-div"),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Input(
+                            placeholder="Sequence A",
+                            type="text",
+                            size="lg",
+                            className="mb-3",
+                            required=True,
+                            id="seq_1",
+                        ),
+                        dbc.Input(
+                            placeholder="Sequence B",
+                            type="text",
+                            size="lg",
+                            className="mb-3",
+                            required=True,
+                            id="seq_2",
+                        ),
+                    ],
+                    width=6,
+                ),
+                dbc.Col(
+                    [
+                        dbc.Input(
+                            placeholder="Match Score (default: 1)",
+                            type="number",
+                            size="lg",
+                            className="mb-3",
+                            id="match_score",
+                        ),
+                        dbc.Input(
+                            placeholder="Mismatch Penalty (default: 1)",
+                            type="number",
+                            size="lg",
+                            className="mb-3",
+                            id="mismatch_penalty",
+                        ),
+                        dbc.Input(
+                            placeholder="Gap Penalty (default: 1)",
+                            type="number",
+                            size="lg",
+                            className="mb-3",
+                            id="gap_penalty",
+                        ),
+                    ],
+                    width=6,
+                ),
+            ]
+        ),
+        dbc.Button("Generate", id="generate_alignment", color="info", size="lg"),
+        html.Pre("(no content)", id="body-div", className="code mb-3 mt-3"),
         html.Div(id="default-alignment-viewer-output"),
         html.Div(id="output-container"),
-    ], fluid=True)
+    ],
+        fluid=True,
+    )
 
 
 HOME_LAYOUT = create_home_layout()
+
